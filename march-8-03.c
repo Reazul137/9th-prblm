47c139a4db13412908cd4fa46c743eb783a9125d
Reazul Islam <reazulislam137@gmail.com>
2018-03-08 22:06:30 +0600

    first commit

---
+++ march-8-03.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int x, y, sum;
+
+    printf("input the first integer  : ");
+    scanf("%d",&x);
+
+    printf("input the second integer :");
+    scanf("%d",&y);
+
+    sum=x+y;
+
+    printf("sum of the above two integers : %d",sum);
+
+    return 0;
+}
